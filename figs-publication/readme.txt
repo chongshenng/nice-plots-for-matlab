54783d2b1fe7fd1003c0aa864d3afdbd773941da
Chong Shen Ng <ngchongshen@gmail.com>
2020-03-30 13:52:51 +0200

    Tweaks for nicer plots

---
+++ figs-publication/readme.txt
@@ -0,0 +1,5 @@
+Steps to combine png and pdf files:
+
+1. In Matlab, run 'plot2D.m'.
+2. In the folder 'figs-publication', compile uavg.tex using pdflatex.
+3. In the same folder, execute 'pdfcrop uavg.pdf' on terminal.
--- plot2D.m
+++ plot2D.m
@@ -1,10 +1,14 @@
 clear all; close all;
 
 %-- Sets a nice background for exporting the final figure
-set(0,'DefaultFigureColor','w',...          % Sets background to white
-    'DefaultAxesColor',[1 1 1],...
-    'DefaultFigureInvertHardCopy','off',... % 'off' preserves on-screen colour
-    'DefaultLineLineWidth',1.5);            % This is to specify line width
+set(0,'DefaultFigureColor','w',...              % Sets figure background to white
+    'DefaultAxesColor','w',...                  % Sets axes background to white
+    'DefaultFigureInvertHardCopy','off',...     % 'off' preserves on-screen colour
+    'DefaultLineLineWidth',0.75,...             % This is to specify line width
+    'DefaultAxesFontName','Times',...           % Secify axes font name
+    'DefaultTextInterpreter','latex',...        % Secify text interpreter
+    'DefaultAxesFontsize',10,...                % Specify axes font size = 10
+    'DefaultLineMarkerSize',10);                % Specify line marker size = 10
 
 %-- Add paths to all subroutines so that you can use them
 addpath(genpath('subroutines'));
@@ -24,7 +28,7 @@ couette_retau220_mean = reshape(data,6,96)';
 
 %-- Now plot the data
 figure(1)
-semilogx(couette_retau220_mean(:,2),couette_retau220_mean(:,3),'b-','linewidth',3);
+h1 = semilogx(couette_retau220_mean(:,2),couette_retau220_mean(:,3),'b-','linewidth',2);
 hold on;
 
 %-- I'm a turbulence researcher, so here's the log-law
@@ -34,8 +38,8 @@ ylin = xlin;
 xlog = logspace(1.0,3.0,50);
 ylog = 1.0/kappa*log(xlog)+5.1;
 figure(1)
-plot(xlin,ylin,'k--');
-plot(xlog,ylog,'k--');
+hlin = plot(xlin,ylin,'k--');
+hlog = plot(xlog,ylog,'k--');
 
 %-- Now format FIGURE 1
 figure(1)
@@ -43,9 +47,29 @@ xlim([1e-1 1e3]);
 ylim([0 25]);
 set(gca,'PlotBoxAspectRatio',[4 3 1]);  % 4:3 is a pleasing aspect ratio
 set(gca,'xtick',logspace(-1,5,7));
-set(gcf,'Position',[100 100 4*125 3*125])
+set(gcf,'Position',[100 100 4*50 3*50])
 xlabel('$x^+$','interpreter','latex');
 ylabel('$\overline{v}^+$','interpreter','latex');
+
+%-- Now put a colored patch here
+%-- Viscous sublayer
+xp = [1e-2 7 7 1e-2 1e-2];
+yp = [0 0 25 25 0];
+hp = patch(xp,yp,[1.0,0.9,0.9]);
+set(hp,'FaceAlpha',.8,'LineStyle','none');
+uistack(hp,'bottom')
+
+%-- Buffer region
+xp = [7 20 20 7 7];
+hp = patch(xp,yp,[0.9,1.0,0.9]);
+set(hp,'FaceAlpha',.8,'LineStyle','none');
+uistack(hp,'bottom')
+
+%-- Log to outer region
+xp = [20 1e3 1e3 20 20];
+hp = patch(xp,yp,[0.9,0.9,1.0]);
+set(hp,'FaceAlpha',.8,'LineStyle','none');
+uistack(hp,'bottom')
 
 %-- For control over text placement
 limx = get(gca,'xlim');
@@ -54,37 +78,55 @@ limx_interval   = log10(limx(2)) - log10(limx(1));
 limy_interval   =       limy(2)  -       limy(1);
 limx_mid        = limx(1) * 10^(0.5 * limx_interval);
 limy_mid        = limy(1) +    (0.5 * limy_interval); 
-text(limx(1)*10^(-0.15*limx_interval),limy(1)+(limy_interval),'(a)')
+t1 = text(limx(1)*10^(-0.15*limx_interval),limy(1)+(limy_interval),'($a$)');
 
 %-- Apply following formats to all figures
 for k = 1
     fig = figure(k);
     set(gca,'ticklength',[0.025 0.025],'layer','top');
-    set(findall(fig,'type','text'),'FontSize',24,'HorizontalAlignment','center')
-    set(findall(fig,'type','axes'),'FontSize',24,'linewidth',1.5)
+    set(findall(fig,'type','text'),'HorizontalAlignment','center')
+    set(findall(fig,'type','axes'),'linewidth',0.75)
+    set(gca,'TickLabelInterpreter','latex');
 end
 
 %% PLOTTING OPTION 1
 %-- I use export_fig which generates beautiful true-screen output
 figure(1);
+%-- First save the background patch as png
+box off; axis off;      % Switches off axes boundaries
+t1.Visible = 'off';     % Switches off text
+h1.Visible = 'off';     % Switches off line plot
+hlin.Visible = 'off';   % Switches off linear plot
+hlog.Visible = 'off';   % Switches off log plot
+export_fig('figs/uavg_raw_bg','-rgb','-png','-m2');
+
+%-- Now save the figure without the patch
+h = findall(gcf,'type','patch');  delete(h);
+box on; axis on;
+t1.Visible = 'on';
+h1.Visible = 'on';
+hlin.Visible = 'on';
+hlog.Visible = 'on';
+set(gcf,'Color','none');
+set(gca,'Color','none');
 export_fig('figs/uavg_raw','-rgb','-pdf');
 
-%% PLOTTING OPTION 2
-%-- For more 'hardcore' control, I use figure_eps, which is a
-%   wrapper for laprint (a LaTeX figure generator) with cleanup tools.
-
-%-- First remove the text interpreters.
-for k = 1
-    fig = figure(k);
-    set(findall(fig,'type','text'),'Interpreter','none')
-end
-
-figw   = 34;
-figh   = figw/4*3;
-figw_m = 0;
-figh_m = 0 ;
-
-%-- Now generate the figure with labels for psfrag
-figure(1)
-figure_eps(figw, figh, figw_m, figh_m, 'figs/uavg_eps');
+% %% PLOTTING OPTION 2
+% %-- For more 'hardcore' control, I use figure_eps, which is a
+% %   wrapper for laprint (a LaTeX figure generator) with cleanup tools.
+% 
+% %-- First remove the text interpreters.
+% for k = 1
+%     fig = figure(k);
+%     set(findall(fig,'type','text'),'Interpreter','none')
+% end
+% 
+% figw   = 34;
+% figh   = figw/4*3;
+% figw_m = 0;
+% figh_m = 0 ;
+% 
+% %-- Now generate the figure with labels for psfrag
+% figure(1)
+% figure_eps(figw, figh, figw_m, figh_m, 'figs/uavg_eps');
     
